7782d7cea7efa2b501ef6410636d14df1209b936
Caio Arriel Souza <caio-arriel@hotmail.com>
2024-02-15 19:58:24 -0300

    Add models & Updating app.py methods POST and GET

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
+# CRUD
+# Create, REad, Update and Delete = Criar, Ler, Atualizar e Deletar
+# Tabela: Tarefa
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message" : "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+                "tasks": task_list,
+                "total_task": 0
+            }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
