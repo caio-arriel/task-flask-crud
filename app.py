4a10e209c471b5e2d48c423347f3cee533dfb56c
Caio Arriel Souza <caio-arriel@hotmail.com>
2024-02-15 20:58:12 -0300

    Updating app.py methods DELETE delete tasks

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def get_task(id):
         if t.id == id:
             return jsonify(t.to_dict())
         
-    return jsonify({"message": "Não foi possível encontrar a atividsade"}), 404
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
     
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
@@ -43,9 +43,11 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
+        
     print(task)
     if task == None:
-        return jsonify({"message": "Não foi possível encontrar a atividsade"}), 404
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
     
     data = request.get_json()
     task.title = data['title']
@@ -53,6 +55,21 @@ def update_task(id):
     task.completed = data['completed']
     print(task)
     return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        print(t.to_dict())
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
